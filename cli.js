6d98ec8d0904f7b31a3cb05809274c8671803297
Bruce W. Herr II <bherr@indiana.edu>
2023-08-21 16:14:11 -0400

    Initial commit

---
+++ cli.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import { createReadStream, createWriteStream, readFileSync } from 'fs';
+import ndjson from 'iterable-ndjson';
+import jsonld from 'jsonld';
+
+// Our CLI Program
+const program = new Command();
+
+program
+  .name('ndjsonld')
+  .description('A CLI for running jsonld commands over ndjson (newline-delimited json) files');
+
+program
+  .command('canonize')
+  .description('Canonize an ndjson file to nquads')
+  .argument('<ndjsonFile>')
+  .argument('<outputQuads>')
+  .option('-c, --context <contextFile>')
+  .option('--unsafe')
+  .action(async (inputFile, outputFile, { context, unsafe }) => {
+    const source = inputFile === '-' ? process.stdin : createReadStream(inputFile, { autoClose: true });
+    const output = outputFile === '-' ? process.stdout : createWriteStream(outputFile, { autoClose: true });
+    const contextObject = JSON.parse(readFileSync(context))?.['@context'];
+
+    for await (const obj of ndjson.parse(source)) {
+      if (contextObject) {
+        obj['@context'] = contextObject;
+      }
+      const nquads = await jsonld.canonize(obj, { safe: !unsafe, compactArrays: false });
+      output.write(nquads);
+    }
+  });
+
+program.parse(process.argv);
