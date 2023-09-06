77331ee5ccaa2e5d0b9b8e93e0afa1a8eb19f11e
Bruce W. Herr II <bherr@indiana.edu>
2023-09-06 07:11:25 -0400

    Ensure blank nodes are unique across documents

--- cli.js
+++ cli.js
@@ -8,9 +8,7 @@ import jsonld from 'jsonld';
 // Our CLI Program
 const program = new Command();
 
-program
-  .name('ndjsonld')
-  .description('A CLI for running jsonld commands over ndjson (newline-delimited json) files');
+program.name('ndjsonld').description('A CLI for running jsonld commands over ndjson (newline-delimited json) files');
 
 program
   .command('canonize')
@@ -24,11 +22,18 @@ program
     const output = outputFile === '-' ? process.stdout : createWriteStream(outputFile, { autoClose: true });
     const contextObject = JSON.parse(readFileSync(context))?.['@context'];
 
+    let docIndex = 0;
     for await (const obj of ndjson.parse(source)) {
+      docIndex++;
       if (contextObject) {
         obj['@context'] = contextObject;
       }
-      const nquads = await jsonld.canonize(obj, { safe: !unsafe, compactArrays: false });
+      const nquads = (await jsonld.canonize(obj, { safe: !unsafe, compactArrays: false }))
+        // Make sure blank nodes are not clashing across documents
+        .replace(
+          /\_\:c14n/g,
+          `_:c14n${docIndex}-`
+        );
       output.write(nquads);
     }
   });
