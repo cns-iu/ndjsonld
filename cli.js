bc62609d6e560005d74007209fbff1a26d5882f1
Bruce W. Herr II <bherr@iu.edu>
2025-04-08 15:32:04 -0400

    Parallelize processing of jsonl files

--- cli.js
+++ cli.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import { createReadStream, createWriteStream, readFileSync } from 'fs';
-import ndjson from 'iterable-ndjson';
 import jsonld from 'jsonld';
+import { createReadStream, createWriteStream, readFileSync } from 'node:fs';
+import { cpus } from 'node:os';
+import { parallelCanonize } from './lib/canonize.js';
+import { readLines } from './lib/read-lines.js';
 
 // Our CLI Program
 const program = new Command();
@@ -15,26 +17,38 @@ program
   .description('Canonize an ndjson file to nquads')
   .argument('<ndjsonFile>')
   .argument('<outputQuads>')
+  .option(
+    '-p, --parallel <numWorkers>',
+    'Number of parallel processes to use. This may result in valid, but out of order data files if n != 1',
+    1
+  )
   .option('-c, --context <contextFile>')
   .option('--unsafe')
-  .action(async (inputFile, outputFile, { context, unsafe }) => {
+  .action(async (inputFile, outputFile, { context, unsafe, parallel }) => {
     const source = inputFile === '-' ? process.stdin : createReadStream(inputFile, { autoClose: true });
     const output = outputFile === '-' ? process.stdout : createWriteStream(outputFile, { autoClose: true });
     const contextObject = JSON.parse(readFileSync(context))?.['@context'];
+    const options = { safe: !unsafe, compactArrays: false };
 
-    let docIndex = 0;
-    for await (const obj of ndjson.parse(source)) {
-      docIndex++;
-      if (contextObject) {
-        obj['@context'] = contextObject;
+    if (parallel == 1) {
+      let docIndex = 0;
+      for await (const line of readLines(source)) {
+        const obj = JSON.parse(line);
+        docIndex++;
+        if (contextObject) {
+          obj['@context'] = contextObject;
+        }
+        const nquads = (await jsonld.canonize(obj, options))
+          // Make sure blank nodes are not clashing across documents
+          .replace(/\_\:c14n/g, `_:c14n${docIndex}-`);
+        output.write(nquads);
       }
-      const nquads = (await jsonld.canonize(obj, { safe: !unsafe, compactArrays: false }))
-        // Make sure blank nodes are not clashing across documents
-        .replace(
-          /\_\:c14n/g,
-          `_:c14n${docIndex}-`
-        );
-      output.write(nquads);
+    } else {
+      if (!parallel || parallel < 1) {
+        parallel = cpus().length;
+      }
+
+      await parallelCanonize(readLines(source), output, options, contextObject, parallel);
     }
   });
 
